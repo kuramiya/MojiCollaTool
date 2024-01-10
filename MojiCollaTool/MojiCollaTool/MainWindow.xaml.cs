53d596679864a8774393b281c5186aa999815bcf
seki <hiroyuki.seki@horiba.com>
2024-01-10 16:12:04 +0900

    VS2022プロジェクト追加 画像読み出し機能、文字追加機能、画像出力機能追加

---
+++ MojiCollaTool/MojiCollaTool/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace MojiCollaTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Moji> mojiList = new List<Moji>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.png;";
+            var dialogResult = openFileDialog.ShowDialog();
+
+            if (dialogResult.HasValue == false || dialogResult.Value == false) return;
+
+            try
+            {
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.UriSource= new Uri(openFileDialog.FileName);
+                bitmapImage.EndInit();
+
+                MainImage.Source = bitmapImage;
+
+                MainCanvas.Width = bitmapImage.Width;
+                MainCanvas.Height = bitmapImage.Height;
+            }
+            catch (Exception ex)
+            {
+                ShowError("Image load error.", ex);
+            }
+        }
+
+        private void ShowError(string message, Exception ex = null)
+        {
+            StringBuilder logMessage = new StringBuilder();
+            logMessage.AppendLine(message);
+            if(ex != null)
+            {
+                logMessage.AppendLine(ex.ToString());
+            }
+
+            File.WriteAllText($"{System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)}\\ErrorLog\\ErrorLog {DateTime.Now:yyyyMMdd-HHmmss}.txt", logMessage.ToString());
+
+            MessageBox.Show($"{message}\r\n({ex.Message})", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+
+        private void AddTextButton_Click(object sender, RoutedEventArgs e)
+        {
+            var moji = new Moji(mojiList.Count + 1);
+
+            mojiList.Add(moji);
+
+            MainCanvas.Children.Add(moji.MojiPanel);
+        }
+
+        private void MainImage_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Point p = e.GetPosition(this);
+        }
+
+        private void OutputImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PNG File|*.png|JPG File|*.jpg";
+            saveFileDialog.FileName = $"MojiColla{DateTime.Now:yyyyMMdd-HHmmss}.png";
+            var dialogResult = saveFileDialog.ShowDialog();
+
+            if (dialogResult.HasValue == false || dialogResult.Value == false) return;
+
+            try
+            {
+                if(saveFileDialog.FileName.EndsWith("jpg"))
+                {
+                    MainCanvas.ToImage(saveFileDialog.FileName, new JpegBitmapEncoder());
+                }
+                else
+                {
+                    MainCanvas.ToImage(saveFileDialog.FileName, new PngBitmapEncoder());
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowError("Image create, output error.", ex);
+            }
+        }
+    }
+}
